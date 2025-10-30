74da5afa89684f7a04623c31692263d3a101c90f
Disk <136144717+sdkv1@users.noreply.github.com>
2025-10-30 20:03:11 +0700

    update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/11zon_cropped-removebg-preview.png',
+  '/11zon_cropped-removebg-preview.png'
 ];
 
 // Install: cache app shell
